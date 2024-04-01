1d4231fd9c07c98ed9c7959352825a0d5c7f592d
celine-schmid <celine.schmid@stud.hslu.ch>
2024-04-01 14:53:59 +0200

    no-overlap

--- assets/javascript/script-2.js
+++ assets/javascript/script-2.js
@@ -56,7 +56,8 @@ function w3AddClass(element, name) {
     var msnry = new Masonry(grid, {
         // options...
         itemSelector: '.grid-item',
-        columnWidth: 10
+        columnWidth: 10,
+        gutterWidth: 10
     });
 
   
